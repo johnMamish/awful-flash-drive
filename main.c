4cb75d54faa4e9ee3fc73e3bc74c54206061596f
John Mamish <john.mamish@gmail.com>
2019-04-18 01:19:47 -0400

    recieve first setup packet from computer

--- main.c
+++ main.c
@@ -7,7 +7,11 @@
 const volatile uint8_t *NVM_SOFTWARE_CAL_AREA = (void*)0x806020;
 
 volatile char_buffer_t sercom3_tx_buf;
-uint8_t sercom3_tx_buf_space[256];
+uint8_t sercom3_tx_buf_space[1024];
+
+static volatile UsbDeviceDescriptor endpoint_descriptors[8] __attribute__((aligned(4)));
+
+uint8_t ep0_buf[64];
 
 void SERCOM3_putch(char ch)
 {
@@ -35,6 +39,19 @@ void SERCOM3_putx(uint32_t x)
 {
     for (int i = 0; i < 8; i++, x <<= 4) {
         uint8_t buux = ((x & 0xf0000000) >> 28);
+        if (buux <= 9) {
+            buux += '0';
+        } else {
+            buux += ('a' - 10);
+        }
+        SERCOM3_putch((char)buux);
+    }
+}
+
+void SERCOM3_putx8(uint8_t x)
+{
+    for (int i = 0; i < 2; i++, x <<= 4) {
+        uint8_t buux = ((x & 0xf0) >> 4);
         if (buux <= 9) {
             buux += '0';
         } else {
@@ -147,10 +164,37 @@ void init_hardware()
     SERCOM3->USART.CTRLA.reg |= (1 << 1);
     SERCOM3->USART.INTENCLR.reg = SERCOM_USART_INTENCLR_DRE;
 
-    // Initialize USB
+    // USB clock configurations. No APBBMASK needed, USB enabled by default.
+    GCLK->CLKCTRL.reg = (1 << 14) | (0 << 8) | (0x06 << 0);
+
+    // Configure GPIOs to work with USB, PA24 -> D-; PA25 -> D+.
+    PORT->Group[0].PMUX[(24 >> 1)].reg = (6 << 4) | (6 << 0);
+    PORT->Group[0].PINCFG[24].reg = 1;
+    PORT->Group[0].PINCFG[25].reg = 1;
+
+    // Load PADCAL registers
+    // todo: mess around by changing / forgetting these values and see what happens
+    USB->DEVICE.PADCAL.bit.TRIM   = (((NVM_SOFTWARE_CAL_AREA[7] & 0b00000011) << 1) |
+                                     ((NVM_SOFTWARE_CAL_AREA[6] & 0b10000000) >> 7));
+    USB->DEVICE.PADCAL.bit.TRIM   = (((NVM_SOFTWARE_CAL_AREA[6] & 0b00000011) << 3) |
+                                     ((NVM_SOFTWARE_CAL_AREA[5] & 0b11100000) >> 5));
+    USB->DEVICE.PADCAL.bit.TRANSP = (((NVM_SOFTWARE_CAL_AREA[6] & 0b01111100) >> 2));
+
+    // Enable USB
+    USB->DEVICE.CTRLA.bit.MODE = USB_CTRLA_MODE_DEVICE;
+    USB->DEVICE.CTRLB.bit.SPDCONF = USB_DEVICE_CTRLB_SPDCONF_FS;
+    USB->DEVICE.CTRLA.bit.ENABLE = 1;
+    USB->DEVICE.DESCADD.reg = (uint32_t)endpoint_descriptors;
+
+    // Configure endpoint 0
+    USB->DEVICE.DeviceEndpoint[0].EPCFG.bit.EPTYPE0 = 1;
+    //USB->DEVICE.DeviceEndpoint[0].;
+    endpoint_descriptors[0].DeviceDescBank[0].ADDR.reg = (uint32_t)ep0_buf;
+    endpoint_descriptors[0].DeviceDescBank[0].PCKSIZE.bit.SIZE = 3;
+    endpoint_descriptors[0].DeviceDescBank[0].PCKSIZE.bit.MULTI_PACKET_SIZE = 0;
 
     // Attach USB hardware
-
+    USB->DEVICE.CTRLB.bit.DETACH = 0;
 }
 
 int main()
@@ -167,14 +211,16 @@ int main()
     // startup PORT peripheral in power manager
     // nothing to do. on by default.
 
-
-    // Wait until usb is attached.
+    // wait until we get a setup packet, then print its contents
+    while (!USB->DEVICE.DeviceEndpoint[0].EPINTFLAG.bit.RXSTP);
+    SERCOM3_puts("got usb setup packet\r\n");
+    for (int i = 0; i < 64; i++) {
+        SERCOM3_putx8(ep0_buf[i]);
+        SERCOM3_putch(' ');
+    }
+    SERCOM3_puts("\r\n");
 
     while(1) {
-        for (volatile int i = 0; i < 100000; i++);
-        for (char i = 'a'; i <= 'z'; i++) {
-            SERCOM3_putch(i);
-        }
-        SERCOM3_puti(3141592653);
+
     }
 }
